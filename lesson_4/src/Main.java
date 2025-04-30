621c1c7644ff8c39056d9c9e899075d75f3275f0
agurokeendavid <agurokeendavid@gmail.com>
2025-04-30 15:40:07 +0800

    add scanner input.

--- lesson_4/src/Main.java
+++ lesson_4/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        NumberClassification classifier = new NumberClassification(-5);
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        NumberClassification classifier = new NumberClassification(number);
         System.out.println(classifier.getFullClassification());
     }
 }
